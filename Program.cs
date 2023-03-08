0d13dba330908660561e0ccbf314378d38198c92
AlejandroMoliGar <89546905+AlejandroMoliGar@users.noreply.github.com>
2023-03-08 17:24:54 -0600

    Secion7 del Taller

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Segunda linea sin salto");
+
+Console.Write("\n");
+
+for (int i = 0; i<=10; i++)
+{
+    Console.Write(i);
+}
+Console.Write("\n");
+
+
+int numeroPersonas = 13;
+string NombrePersonas = "Alejandro";
+Console.WriteLine("En este Cuarto hay {0} Personas y una se llama {1}", numeroPersonas ,NombrePersonas );
