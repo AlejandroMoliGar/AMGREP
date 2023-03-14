d283c2c47471d73d7447d38d98bc1acc9a5afc7d
AlejandroMoliGar <89546905+AlejandroMoliGar@users.noreply.github.com>
2023-03-13 20:27:19 -0600

    Practica 11 y 12

--- seccion11/Program.cs
+++ seccion11/Program.cs
@@ -1,10 +1,21 @@
-﻿var resultado = suma(6,7);
-
+﻿var resultado = Suma(6,7);
+Print (resultado);
+Print (Suma(5,6));
 Console.WriteLine("Resultado = {0}", resultado);
 
-int suma (int x,int y)
+int Suma (int x,int y)
 {
     return x +y;
 }
 
-int Resta
+ int Resta( int x, int y){
+    return x - y;
+ }
+
+ int Multiplicacion ( int x, int y){
+    return x * y;
+ }
+
+ void Print (int resultado){
+    Console.WriteLine("Resultado = {0}", resultado);
+ }
---
+++ seccion12/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1;
+int num2;
+int num3;
+
+Console.WriteLine("Introduce el numero 1");
+num1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el numero 1");
+num2 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el numero 1");
+num3 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("La Suma es = {0}",num1+num2+num3);
