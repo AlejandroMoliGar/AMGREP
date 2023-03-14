4ed725ed048c6f2e04bd255e79f36a1174a69e02
AlejandroMoliGar <89546905+AlejandroMoliGar@users.noreply.github.com>
2023-03-13 19:17:33 -0600

    Practicas de taller

---
+++ Seccion_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+sbyte numeroMinimoByteSigno = -20;
+
+
+byte numeroMinimoByteSinSigno = 150;
+
+
+Console.Write(numeroMinimoByteSigno);
+Console.Write("\n");
+Console.Write(numeroMinimoByteSinSigno);
+
+
+numeroMinimoByteSigno = (sbyte) numeroMinimoByteSinSigno;
+Console.Write("\n");
+Console.Write(numeroMinimoByteSigno);
---
+++ seccion11/Program.cs
@@ -0,0 +1,10 @@
+﻿var resultado = suma(6,7);
+
+Console.WriteLine("Resultado = {0}", resultado);
+
+int suma (int x,int y)
+{
+    return x +y;
+}
+
+int Resta
