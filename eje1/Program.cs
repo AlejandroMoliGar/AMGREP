4ff37680053a9f6b9a99f7712d587bdf638f6e9b
AlejandroMoliGar <89546905+AlejandroMoliGar@users.noreply.github.com>
2023-03-15 17:11:53 -0600

    Primeros Ejercicios de la sesion 7

---
+++ eje1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+        double diff = Math.Abs(num1 - num2);
+        if (num1 > num2)
+        {
+            diff *= 2;
+        }
+        Console.WriteLine("Diferencia absoluta: " + diff);
+
+        for (int i = 1; i <= 99; i++){
+            if (i % 2 != 0){
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Numeros {0}", i);
+        }
+            
+        }
+    }
