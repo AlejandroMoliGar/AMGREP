1a4973aec1329cc5b767a0ad4745e94c3d567483
AlejandroMoliGar <89546905+AlejandroMoliGar@users.noreply.github.com>
2023-03-15 20:02:42 -0600

    Ejercicios de la sesion 8

---
+++ eje2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Ingrese el ancho del rectángulo: ");
+        double ancho = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la altura del rectángulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double perimetro = 2 * (ancho + altura);
+        double area = ancho * altura;
+        double diagonal = Math.Sqrt(ancho * ancho + altura * altura);
+
+        Console.WriteLine("Perímetro: " + perimetro);
+        Console.WriteLine("Área: " + area);
+        Console.WriteLine("Diagonal: " + diagonal);
+    }
+}
---
+++ eje21/Program.cs
@@ -0,0 +1,14 @@
+﻿using eje21;
+
+Student student = new Student();
+student.SetAge(24);
+student.SetName("Alejandro");
+student.SayHello();
+student.GoToClasses();
+student.ShowAge();
+
+Teacher teacher = new Teacher();
+teacher.SetAge(30);
+teacher.SetName("Eduardo");
+teacher.SayHello();
+teacher.Explain();
